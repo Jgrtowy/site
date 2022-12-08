f58ea23193f2999ba1ad0bf9443005aea9c9fe7f
Jgrtowy <panjogurcik@outlook.com>
2022-12-08 20:06:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useRef, useEffect } from 'React'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header>
+      Jgrtowy's site
+    </header>
+      <div id='main'>
+        <img src="/public/under.png" />
+          <h1>Under construction, check back later!</h1>
+      </div>
+    </>
+  )
+}
+
+
+
+export default App
